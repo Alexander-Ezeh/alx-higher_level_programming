30e4721a5bbb7880c0cec453c06caf15e39a0c80
Alexander Ikennaefuna Ezeh <122819061+Alexander-Ezeh@users.noreply.github.com>
2024-03-13 15:23:06 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # Author - Alexander Ikennaefuna Exeh
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(122, 96, -1):
+    if (i % 2 == 0):
+        print("{}".format(chr(i)), end="")
+    elif (i % 2 != 0):
+        print("{}".format(chr(i-32)), end="")
