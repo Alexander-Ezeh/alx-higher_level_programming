306e00dfd0883ad47fcb3e8138897035d5b238d6
Alexander Ikennaefuna Ezeh <122819061+Alexander-Ezeh@users.noreply.github.com>
2024-03-14 21:21:05 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
 
-import dis
-
 def magic_calculation(a, b):
+  
     from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+        return (c)
 
-# Uncomment the following lines to see the bytecode of the function
-# dis.dis(magic_calculation)
+    else:
+        return(sub(a, b))
+
+# import dis
+
+# def magic_calculation(a, b):
+#     from magic_calculation_102 import add, sub
+
+#     if a < b:
+#         c = add(a, b)
+#         for i in range(4, 6):
+#             c = add(c, i)
+#         return c
+#     else:
+#         return sub(a, b)
+
+# # Uncomment the following lines to see the bytecode of the function
+# # dis.dis(magic_calculation)
