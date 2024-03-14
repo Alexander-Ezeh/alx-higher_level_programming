60257a70f359df117fb983771e5380b10ae85363
Alexander Ikennaefuna Ezeh <122819061+Alexander-Ezeh@users.noreply.github.com>
2024-03-14 21:39:56 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
+
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if "_" not in name[:2]:
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith("__"):
             print(name)
